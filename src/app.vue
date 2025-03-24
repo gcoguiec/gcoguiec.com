a9819d6a5c02ccf02fbfce0ca3ce12735d0078de
Guillaume 'Yugo' Coguiec <mail@gcoguiec.com>
2025-03-24 11:01:04 -0400

    wip: Starting the work on themes.

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,74 @@
+<script lang="ts" setup>
+import { useHeadSafe } from '@unhead/vue';
+import { useI18n } from 'vue-i18n';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const { t } = useI18n();
+
+useHeadSafe({
+  templateParams: {
+    site: {
+      name: 'Guillaume Coguiec'
+    },
+    separator: '—'
+  },
+  titleTemplate: '%site.name %separator %s',
+  meta: [
+    { name: 'description', content: t('description') },
+    { name: 'author', content: 'Guillaume Coguiec' },
+    { name: 'og:type', content: 'website' },
+    { name: 'og:site_name', content: 'gcoguiec.com' },
+    { name: 'og:image', content: '/social-banner.jpg' },
+    {
+      name: 'og:url',
+      content: () =>
+        `${import.meta.env['url'] ?? 'http://localhost:3000'}${route.path}`
+    }
+  ]
+});
+</script>
+
 <template>
   <router-view />
 </template>
 
 <style lang="scss">
+@use 'sass:map';
+
+@use '~/assets/scss/constants';
 @use '~/assets/scss/bem';
+
+$themes: (
+  dark: (
+    background-color: constants.$tuna
+  ),
+  light: (
+    background-color: white
+  )
+);
+
+@mixin app-theme($options) {
+  background-color: map.get($options, 'background-color');
+}
+
+@include bem.block('theme') {
+  &:not(#{bem.modifier_selector('user-set')}) {
+    @each $theme, $options in $themes {
+      @media (prefers-color-scheme: $theme) {
+        color-scheme: $theme;
+        @include app-theme($options);
+      }
+    }
+  }
+
+  @each $theme, $options in $themes {
+    @include bem.modifier($theme) {
+      color-scheme: $theme;
+      @include app-theme($options);
+    }
+  }
+}
 
 @include bem.block('app') {
   display: flex;
