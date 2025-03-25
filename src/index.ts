662b8880de9a4ba84998f4768bf6ebd4c7f8d983
Guillaume 'Yugo' Coguiec <mail@gcoguiec.com>
2025-03-25 12:25:11 -0400

    feat: Register store with the app.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import { setupLayouts } from 'virtual:generated-layouts';
 
 import '~/normalizer.scss';
 import App from '~/app.vue';
+import { createPinia, type StateTree } from 'pinia';
+import { useStore } from '~/store/root';
+import devalue from '@nuxt/devalue';
 
 const i18n = createI18n({
   legacy: false,
@@ -26,11 +29,32 @@ export const createApp = ViteSSG(
       }
     ])
   },
-  ({ app }) => {
+  ({ app, initialState, router }) => {
+    const pinia = createPinia();
+    app.use(pinia);
     app.use(i18n);
+
+    if (import.meta.env.SSR) {
+      initialState = {
+        pinia: pinia.state.value
+      };
+    } else {
+      pinia.state.value = (initialState['pinia'] ?? {}) as Record<
+        string,
+        StateTree
+      >;
+    }
+
+    router.beforeEach((_to, _from, next) => {
+      useStore(pinia);
+      next();
+    });
   },
   {
     rootContainer: '#gc',
-    useHead: true
+    useHead: true,
+    transformState(state): unknown {
+      return import.meta.env.SSR ? devalue(state) : state;
+    }
   }
 );
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,22 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+import { useStore } from '~/store/root';
+import { useBem } from '~/composables/use-bem';
+
+const { block } = useBem('layout');
+const store = useStore();
+
+const blockClass = computed(() =>
+  block({
+    default: true,
+    [store.theme]: true
+  })
+);
+</script>
+
 <template>
-  <div>
+  <div :class="blockClass">
     <router-view />
   </div>
 </template>
